5c7fcabfed1c1270f179adfb521355bbb2bfd925
Gonzalo Romero <gondragon13@gmail.com>
2019-10-25 00:26:35 -0300

    Añadida página Tabla

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
+// Inicializar Carousel Principal --> Probablemente sea preferible moverlo a un tag script en la página principal.
 $('#carouselInfo').on('slide.bs.carousel', {carouselname: "#carouselInfo"},function (event) {
   $(event.data.carouselname + " .nav .nav-link").eq(event.to).toggleClass("active");
   $(event.data.carouselname + " .nav .nav-link").eq(event.from).toggleClass("active");
+})
+
+// Inicializar los tooltips.
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
 })
